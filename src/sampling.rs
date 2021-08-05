45ffae687421ce919564aa88fa50bfb3adfcade0
Yruama_Lairba <yruama_lairba@hotmail.com>
2021-08-05 04:48:29 +0200

    document the sampling module

--- src/sampling.rs
+++ src/sampling.rs
@@ -1,10 +1,82 @@
-//! Sampling configuration
+//! Command builder for sampling configuration.
 //!
-//! Not safe at the moment
-//! TODO:
-//!  - at the moment, usb mode and bosr = 1 and not writing sr produce an invalid configuration,
-//! Enforcing sr write seems a good idea.
-//!  - have more meaningfull additional api would be welcome.
+//! This module offer two style for building a command, a nice style using a master clock and a raw
+//! style allowing more advance use.
+//!
+//! # Style with master clock
+//! With this method, the builder is instantiated with a marker to indicate the internal master clock
+//! frequency. Valid markers are :
+//!  - `Mclk12M288` for a 12.288 MHz master clock
+//!  - `Mclk18M432` for a 18.432 MHz master clock
+//!  - `Mclk11M2896` for a 11.2896 MHz master clock
+//!  - `Mclk16M9344` for a 16.9344 MHz master clock
+//!  - `Mclk12M` for a 12MHz master clock, correspond to USB mode.
+//!
+//! You also don't write directly to USB/NORMAL, BOSR, or SR fields. Instead, you use a *virtual*
+//! SampleRate field that do it for you.
+//!
+//!  ## Example
+//! ```
+//! # use wm8731_alt::sampling::*;
+//! //instanciate the builder
+//! let cmd = sampling_command_builder_mclk(Mclk12M288);
+//! //setup the sampling rate
+//! let cmd = cmd.sample_rate().adc48k_dac48k();
+//! //build the command
+//! let cmd = cmd.into_command();
+//! ```
+//!
+//! # Raw style
+//! With this method, you write directly to USB/NORMAL, BOSR, and SR fields. This way is useful for
+//! case not handled by the other method. Notably, the Sr field writer don't have very meaningful
+//! method name, because same combination of USB/NORMAL, BOSR, and SR can produce different
+//! sampling rate by just changing the master clock. Look the
+//! [WAN0117](https://statics.cirrus.com/pubs/appNote/WAN0117.pdf) application notice for
+//! advanced sampling rate selection.
+//!
+//! ## Example
+//! ```
+//! # use wm8731_alt::sampling::*;
+//! //instantiate the builder
+//! let cmd = sampling_command_builder();
+//! //normal mode operation
+//! let cmd = cmd.usb_normal().normal();
+//! //write bosr bit
+//! let cmd = cmd.bosr().clear_bit();
+//! //write sr field
+//! let cmd = cmd.sr().sr_0b0000();
+//! //build the command
+//! let cmd = cmd.into_command();
+//! ```
+//!
+//! # Safety and coherence
+//! For safety and coherence some reason, a compile time check is done to enforce some
+//! manipulations and prohibit others. To avoid compilation error from this checks, a good
+//! guideline is to always setup explicitly USB/NORMAL, BOSR first and then setup SR for the raw
+//! style, and explicitly setup SampleRate fields for the style with master clock. don't forget to
+//! check with the datasheet if values are good.
+//!
+//! ## Example
+//! ```
+//! # #[cfg(any())] //avoid some compilation error when testing doc
+//! # {
+//! # use wm8731_alt::sampling::*;
+//! //error, sample rate require to be explicitly set
+//! let cmd = sampling_command_builder_mclk(Mclk12M288).into_command();
+//! //error, this sampling rate setup is impossible with the current master clock
+//! let cmd = sampling_command_builder_mclk(Mclk12M288).sample_rate().adc44k1_dac44k1();
+//! //error, sr require to be explicitly set
+//! let cmd = sampling_command_builder().into_command();
+//! //error, USB/NORMAL and BOSR need to be set before SR because it affect SR validity
+//! let cmd = sampling_command_builder().sr().sr_0b0000();
+//! //error, cannot change USB/NORMAL and BOSR after SR because it affect SR validity
+//! let cmd =
+//! sampling_command_builder().usb_normal().usb().bosr().clear_bit().sr().sr_0b0000().bosr().set_bit();
+//! //error, USB/NORMAL, BOSR, SR combination is invalid
+//! let cmd =
+//! sampling_command_builder().usb_normal().usb().bosr().set_bit().sr().sr_0b0000();
+//! # }
+//!
 //!
 #![allow(clippy::new_without_default)]
 
@@ -73,7 +145,7 @@ pub fn sampling_command_builder() -> Sampling<(Unset, Unset, Unset)> {
 }
 
 /// Instanciate a command builder to set sampling configuration for a particular master clock.
-pub fn sampling_command_builder_mclk<MCLK>() -> Sampling<(MCLK, Unset)>
+pub fn sampling_command_builder_mclk<MCLK>(_: MCLK) -> Sampling<(MCLK, Unset)>
 where
     MCLK: Mclk,
 {
@@ -342,6 +414,17 @@ impl<MODE, BOSR> Sampling<(MODE, BOSR, SrIsSet)> {
     }
 }
 
+//Once SampleRate have been explicitly set, a valid command can be instanciated
+impl<MCLK> Sampling<(MCLK, SrIsSet)> {
+    /// Instanciate a command
+    pub fn into_command(self) -> Command<()> {
+        Command::<()> {
+            data: self.data,
+            t: PhantomData::<()>,
+        }
+    }
+}
+
 //make the normal/usb mode only settable once (over constraint?)
 //prevent to change normal/ub mode once sr is set
 impl<BOSR> Sampling<(Unset, BOSR, Unset)> {
@@ -596,7 +679,7 @@ mod tests {
         //should warn, you may think you change the command but this is not the case
         new_cmd.usb_normal().normal();
 
-        let cmd = sampling_command_builder_mclk::<Mclk12M288>();
+        let cmd = sampling_command_builder_mclk(Mclk12M288);
         //should warn, you may think you change the command but this is not the case
         cmd.sample_rate().adc48k_dac48k();
     }
