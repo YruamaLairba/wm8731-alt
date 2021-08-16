8f780099ff38ea8d0e7b8fbb0756574dd47c95a9
Yruama_Lairba <yruama_lairba@hotmail.com>
2021-08-16 12:03:49 +0200

    creat module interface for communication details

---
+++ src/interface.rs
@@ -0,0 +1,119 @@
+//! Handle communication details
+use core::marker::PhantomData;
+use embedded_hal::blocking::{i2c, spi};
+use embedded_hal::digital::v2::OutputPin;
+
+use crate::Command;
+
+///Represent a frame sended through I2C or SPI interface.
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub struct Frame {
+    data: u16,
+}
+
+impl From<Frame> for [u8; 2] {
+    ///Allow to convert frame to an array directly usable with SPI and I2C abstraction from embedded-hal.
+    fn from(frame: Frame) -> [u8; 2] {
+        frame.data.to_be_bytes()
+    }
+}
+
+impl From<Frame> for [u16; 1] {
+    ///Allow to convert frame to an array directly usable with 16 bit word SPI abstraction from embedded-hal.
+    fn from(frame: Frame) -> [u16; 1] {
+        [frame.data]
+    }
+}
+
+impl From<Frame> for u16 {
+    ///Allow to convert frame in u16.
+    fn from(frame: Frame) -> u16 {
+        frame.data
+    }
+}
+
+impl<T> From<Command<T>> for Frame {
+    ///Allow to convert frame to an array directly usable with SPI and I2C abstraction from embedded-hal.
+    fn from(cmd: Command<T>) -> Frame {
+        Frame { data: cmd.data }
+    }
+}
+
+/// Serial Interface abstraction for the wm8731 generic driver.
+pub trait WriteFrame {
+    fn send(&mut self, frame: Frame);
+}
+
+/// Generic blocking I2C communication implementation using embedded-hal.
+pub struct I2CInterface<I2C> {
+    i2c: I2C,
+    address: u8,
+}
+
+impl<I2C> I2CInterface<I2C>
+where
+    I2C: i2c::Write,
+{
+    pub fn new(i2c: I2C, address: u8) -> Self {
+        Self { i2c, address }
+    }
+    pub fn release(self) -> I2C {
+        self.i2c
+    }
+}
+
+impl<I2C> WriteFrame for I2CInterface<I2C>
+where
+    I2C: i2c::Write,
+{
+    fn send(&mut self, frame: Frame) {
+        let frame: [u8; 2] = frame.into();
+        let _ = self.i2c.write(self.address, &frame);
+    }
+}
+
+/// Generic blocking SPI communication implementation using embedded-hal.
+pub struct SPIInterface<SPI, CS, W> {
+    spi: SPI,
+    cs: CS,
+    w: PhantomData<W>,
+}
+
+impl<SPI, CS, W> SPIInterface<SPI, CS, W> {
+    pub fn new(spi: SPI, cs: CS) -> Self {
+        Self {
+            spi,
+            cs,
+            w: PhantomData::<W>,
+        }
+    }
+    pub fn release(self) -> SPI {
+        self.spi
+    }
+}
+
+impl<SPI, CS> WriteFrame for SPIInterface<SPI, CS, u8>
+where
+    SPI: spi::Write<u8>,
+    CS: OutputPin,
+{
+    fn send(&mut self, frame: Frame) {
+        let frame: [u8; 2] = frame.into();
+        let _ = self.cs.set_low();
+        let _ = self.spi.write(&frame);
+        let _ = self.cs.set_high();
+    }
+}
+
+impl<SPI, CS> WriteFrame for SPIInterface<SPI, CS, u16>
+where
+    SPI: spi::Write<u16>,
+    CS: OutputPin,
+{
+    fn send(&mut self, frame: Frame) {
+        let frame: [u16; 1] = frame.into();
+        let _ = self.cs.set_low();
+        let _ = self.spi.write(&frame);
+        let _ = self.cs.set_high();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 use core::marker::PhantomData;
-use embedded_hal::blocking::{i2c, spi};
-use embedded_hal::digital::v2::OutputPin;
+
+use crate::interface::WriteFrame;
 
 #[macro_use]
 mod macros;
@@ -11,6 +11,7 @@ pub mod analogue_audio_path;
 pub mod digital_audio_interface;
 pub mod digital_audio_path;
 pub mod headphone_out;
+pub mod interface;
 pub mod line_in;
 pub mod power_down;
 pub mod sampling;
@@ -75,119 +76,6 @@ impl<T> Clone for Command<T> {
     }
 }
 
-impl<T> From<Command<T>> for Frame {
-    ///Allow to convert frame to an array directly usable with SPI and I2C abstraction from embedded-hal.
-    fn from(cmd: Command<T>) -> Frame {
-        Frame { data: cmd.data }
-    }
-}
-
-///Represent a frame sended through I2C or SPI interface.
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-pub struct Frame {
-    data: u16,
-}
-
-impl From<Frame> for [u8; 2] {
-    ///Allow to convert frame to an array directly usable with SPI and I2C abstraction from embedded-hal.
-    fn from(frame: Frame) -> [u8; 2] {
-        frame.data.to_be_bytes()
-    }
-}
-
-impl From<Frame> for [u16; 1] {
-    ///Allow to convert frame to an array directly usable with 16 bit word SPI abstraction from embedded-hal.
-    fn from(frame: Frame) -> [u16; 1] {
-        [frame.data]
-    }
-}
-
-impl From<Frame> for u16 {
-    ///Allow to convert frame in u16.
-    fn from(frame: Frame) -> u16 {
-        frame.data
-    }
-}
-
-/// Serial Interface abstraction for the wm8731 generic driver.
-pub trait WriteFrame {
-    fn send(&mut self, frame: Frame);
-}
-
-/// Generic blocking I2C communication implementation using embedded-hal.
-pub struct I2CInterface<I2C> {
-    i2c: I2C,
-    address: u8,
-}
-
-impl<I2C> I2CInterface<I2C>
-where
-    I2C: i2c::Write,
-{
-    pub fn new(i2c: I2C, address: u8) -> Self {
-        Self { i2c, address }
-    }
-    pub fn release(self) -> I2C {
-        self.i2c
-    }
-}
-
-impl<I2C> WriteFrame for I2CInterface<I2C>
-where
-    I2C: i2c::Write,
-{
-    fn send(&mut self, frame: Frame) {
-        let frame: [u8; 2] = frame.into();
-        let _ = self.i2c.write(self.address, &frame);
-    }
-}
-
-/// Generic blocking SPI communication implementation using embedded-hal.
-pub struct SPIInterface<SPI, CS, W> {
-    spi: SPI,
-    cs: CS,
-    w: PhantomData<W>,
-}
-
-impl<SPI, CS, W> SPIInterface<SPI, CS, W> {
-    pub fn new(spi: SPI, cs: CS) -> Self {
-        Self {
-            spi,
-            cs,
-            w: PhantomData::<W>,
-        }
-    }
-    pub fn release(self) -> SPI {
-        self.spi
-    }
-}
-
-impl<SPI, CS> WriteFrame for SPIInterface<SPI, CS, u8>
-where
-    SPI: spi::Write<u8>,
-    CS: OutputPin,
-{
-    fn send(&mut self, frame: Frame) {
-        let frame: [u8; 2] = frame.into();
-        let _ = self.cs.set_low();
-        let _ = self.spi.write(&frame);
-        let _ = self.cs.set_high();
-    }
-}
-
-impl<SPI, CS> WriteFrame for SPIInterface<SPI, CS, u16>
-where
-    SPI: spi::Write<u16>,
-    CS: OutputPin,
-{
-    fn send(&mut self, frame: Frame) {
-        let frame: [u16; 1] = frame.into();
-        let _ = self.cs.set_low();
-        let _ = self.spi.write(&frame);
-        let _ = self.cs.set_high();
-    }
-}
-
 pub struct Wm8731<I> {
     interface: I,
 }
@@ -241,11 +129,13 @@ mod tests {
     }
 
     fn _should_compile() {
+        use crate::interface::SPIInterface;
         let spi_if: SPIInterface<_, _, u8> = SPIInterface::new(FakeSpi, FakePin);
         let _wm8731 = Wm8731::new(spi_if);
     }
     #[cfg(any())]
     fn _should_not_compile() {
+        use crate::interface::SPIInterface;
         use crate::reset::*;
         let mut spi_if: SPIInterface<_, _, u8> = SPIInterface::new(FakeSpi, FakePin);
         //forbidden to encourage using the driver instead serial interface
